69694edb50c303ed23021cd566c8ce7e49da5113
TIAN HAOTIAN <flkbme@gmail>
2023-09-04 21:03:42 +0800

    update to day74

--- test.py
+++ test.py
@@ -1,24 +1,9 @@
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def find_age():
-    for age in range(1, 101):
-        childhood_age = age // 14
-        adult_age = childhood_age + age // 7
-        marriage_age = adult_age + age // 6
-        son_birth_age = marriage_age + 3
-
-        if son_birth_age >= 7 and is_prime(son_birth_age ** 2 - son_birth_age):
-            return marriage_age
-
-    return None
-
-
-age_at_marriage = find_age()
-print("张先生结婚时的年龄是:", age_at_marriage, "岁")
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(-8, 8, 1024)
+y1 = 0.618 * np.abs(x) - 0.8 * np.sqrt(64 - x ** 2)
+y2 = 0.618 * np.abs(x) + 0.8 * np.sqrt(64 - x ** 2)
+plt.plot(x, y1, color='r')
+plt.plot(x, y2, color='r')
+plt.title("我爱C语言中文网",fontsize=20,color="b")
+plt.show()
